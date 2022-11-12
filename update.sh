7578f0a21be5495e40928275e9455b0c50323439
Rowan Ackerman <70336726+codeBodger@users.noreply.github.com>
2022-11-12 22:23:39 +0000

    Mostly finished update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+if [ -f .update ]; then
+	# rm .update
+	echo hi
+fi
+
+if [ -f .gitignore ]; then
+	# rm .gitignore
+	echo hi
+fi
+
+FILES=$(curl "https://raw.githubusercontent.com/codeBodger/Drops/main/.update")
+for FILE in $FILES; do
+	cat <<< $(curl "https://raw.githubusercontent.com/codeBodger/Drops/main/$FILE") > ".$FILE"
+done
